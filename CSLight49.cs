2aefd8ed1984eac9a1f4e3df8b7ac86ef6cdf292
gabhar-01 <shevchenko.lilya@mail.ru>
2022-04-04 23:47:46 +0700

    Create CSLight49.cs

---
+++ CSLight49.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CSLight49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = 30;
+            int arrayMinValue = 0;
+            int arrayMaxValue = 20;
+            
+            Random random = new Random();
+            int[] array = new int[arrayLength];
+
+            int firstArrayElementIndex = 0;
+            int secondArrayElementIndex = firstArrayElementIndex + 1;
+            int lastArrayElementIndex = array.Length - 1;
+            int secondLastArrayElementIndex = lastArrayElementIndex - 1;
+
+            for (int i = firstArrayElementIndex; i <= lastArrayElementIndex; i++)
+            {
+                array[i] = random.Next(arrayMinValue, arrayMaxValue);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.Write("\nЛокальные максимумы массива: ");
+
+            if (array[firstArrayElementIndex] >= array[secondArrayElementIndex])
+            {
+                Console.Write(array[firstArrayElementIndex] + " ");
+            }
+
+            for (int i = secondArrayElementIndex; i <= secondLastArrayElementIndex; i++)
+            {
+                if (array[i] >= array[i - 1] && array[i] >= array[i + 1])
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+            if (array[lastArrayElementIndex] >= array[secondLastArrayElementIndex])
+            {
+                Console.Write(array[lastArrayElementIndex]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
