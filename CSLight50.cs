801260afe77f04abc00f81fd14b5140a4c38aee9
gabhar-01 <shevchenko.lilya@mail.ru>
2022-04-05 01:03:44 +0700

    Create CSLight50.cs

---
+++ CSLight50.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CSLight50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            bool isRunning = true;
+
+            string sumCommand = "sum";
+            int sum = 0;
+            string exitCommand = "exit";
+
+            int[] array = new int[0];
+
+            Console.WriteLine("Введите числа для добавления их в массив." +
+                "\nВведите команду sum для вывода суммы всех введенных чисел." +
+                "\nВведите команду exit для выхода из программы.");
+
+             while (isRunning)
+             {
+                userInput = Console.ReadLine();
+
+                if (userInput == sumCommand)
+                {
+                    foreach (int number in array)
+                    {
+                        sum += number;
+                    }
+                    Console.WriteLine(sum);
+                    sum = 0;
+                }
+                else if (userInput == exitCommand)
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    int[] tempArray = new int[array.Length + 1];
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        tempArray[i] = array[i];
+                    }
+                    tempArray[tempArray.Length - 1] = Convert.ToInt32(userInput);
+                    array = tempArray;
+                }
+             }
+        }
+    }
+}
