905d844846b889ed9fbaf11b88989f98be63714a
gabhar-01 <shevchenko.lilya@mail.ru>
2022-04-05 14:24:01 +0700

    Create CSLight208.cs

---
+++ CSLight208.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSLight208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Перестаньте убивать селёдку ради шуб!";
+            string[] separateWords = phrase.Split(' ');
+
+            foreach (var oneWord in separateWords)
+            {
+                Console.WriteLine(oneWord);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
